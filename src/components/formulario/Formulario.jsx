7d58a8d122720b3941c523133826814cd3a370a6
victor <napiervg333@gmail.com>
2022-03-08 12:58:19 -0300

    formulario nuevo cliente terminado

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -1,46 +1,111 @@
-import { Formik,Form,Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Formulario = () => {
+
+  const nuevoClienteSchema = Yup.object().shape({
+    nombre: Yup.string()
+               .required("El Nombre del Cliente es Obligatorio")
+               .min(3, "El Nombre es muy Corto")
+               .max(40, "El Nombre es muy Largo"),
+    empresa: Yup.string()
+                .required("El nombre de la empresa es Obligatorio"),
+    email: Yup.string()
+              .required("El Email del Cliente es Obligatorio")
+              .email("Email no Valido"),
+    telefono: Yup.number()
+                 .typeError("Numero no Valido")
+                 .integer("Numero no Valido")
+                 .positive("Numero no Valido")
+                 .required("El Numero del Cliente es Obligatorio"),
+    notas: Yup.string()
+       
+  });
+
     return ( 
         <div className="contenedor__titleForm">
           <h3 className="titleForm">Agregar Cliente</h3>
-          <Formik>
+          <Formik
+
+            //valores iniciales de mi obejto
+            initialValues={{
+              nombre:"",
+              empresa:"",
+              email:"",
+              telefono:"",
+              notas:""
+            }}
+
+            //shema de YUP
+            validationSchema={nuevoClienteSchema}
+            
+            //envio el formulario
+            onSubmit={(values,{resetForm})=>{
+              resetForm();
+              console.log(values);
+            }}
+          >
+            {({errors})=>(
+
               <Form>
-                  <div>
+                  <div className="camposForm">
                       <label>Nombre: </label>
                       <Field
                         type="text"
+                        placeholder="Nombre del Cliente"
+                        name="nombre" 
                       />
+                      <ErrorMessage name="nombre" component={()=>(<p className="alerta">{errors.nombre}</p>)}/>
                   </div>
-                  <div>
+                  <div className="camposForm">
                       <label>Empresa: </label>
                       <Field
                         type="text"
+                        placeholder="Empresa del Cliente"
+                        name="empresa"
                       />
+                      <ErrorMessage name="empresa" component={()=>(<p className="alerta">{errors.empresa}</p>)}/>
                   </div>
-                  <div>
+                  <div className="camposForm">
                       <label>E-mail: </label>
                       <Field
                         type="email"
-                        className=""
+                        placeholder="Email del Cliente"
+                        name="email"
                       />
+                      <ErrorMessage name="email" component={()=>(<p className="alerta">{errors.email}</p>)}/>
                   </div>
-                  <div>
+                  <div className="camposForm">
                       <label>Telefono: </label>
                       <Field
                         type="tel"
-                        className=""
+                        placeholder="Telefono del Cliente"
+                        name="telefono"
                       />
+                      <ErrorMessage name="telefono" component={()=>(<p className="alerta">{errors.telefono}</p>)}/>
                   </div>
-                  <div>
+                  <div className="camposForm">
                       <label>Notas: </label>
                       <Field
                         as="textarea"
                         type="text"
-                        className=""
+                        placeholder="Notas del Cliente"
+                        name="notas"
                       />
+                      <ErrorMessage name="notas" component={()=>(<p className="alerta">{errors.notas}</p>)}/>
+                  </div>
+
+                  <div className="campoBtn">
+                      <button
+                        type="submit"
+                        className="btnForm"
+                      >
+                        Enviar
+                      </button>
                   </div>
               </Form>
+
+            )}
           </Formik>
         </div>
      );
